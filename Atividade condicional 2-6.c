d3f224a4d3ff92fe2c6184ed8a036469349b2791
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-11 21:20:14 -0300

    Add files via upload

---
+++ Atividade condicional 2-6.c
@@ -0,0 +1,26 @@
+//
+#include <stdio.h>
+
+int main() {
+    float altura, pesoIdeal;
+    char sexo;
+
+    printf("Digite sua altura em metros: ");
+    scanf("%f", &altura);
+
+    printf("Digite seu sexo (M ou F): ");
+    scanf(" %c", &sexo);
+
+    if (sexo == 'M' || sexo == 'm') {
+        pesoIdeal = (72.7 * altura) - 58;
+        printf("Seu peso ideal eh %.2f kg\n", pesoIdeal);
+    } else if (sexo == 'F' || sexo == 'f') {
+        pesoIdeal = (62.1 * altura) - 44.7;
+        printf("Seu peso ideal eh %.2f kg\n", pesoIdeal);
+    } else {
+        printf("Sexo invalido.\n");
+    }
+
+    return 0;
+}
+
